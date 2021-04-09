1b96cd216b7c734df65510f1edd302d8f28cad5f
Shmish <shmish90@gmail.com>
2021-04-08 22:56:43 -0400

    removed reduntant imports and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import SubmitClaim from "./components/SubmitClaim";
 import ViewClaim from "./components/ViewClaim";
 import Success from "./components/Sucess";
 import Claim from "./components/Claim";
-import Claims from "./components/Claims";
 import SearchClaim from "./components/SearchClaim";
 
 function App() {
@@ -16,8 +15,7 @@ function App() {
       <Route path="/SubmitClaim" component={SubmitClaim} />
       <Route path="/ViewClaim" component={ViewClaim} />
       <Route path="/Success" component={Success} />
-      <Route path="/Claim" component={Claim} />
-      <Route path="/Claims" component={Claims} />
+      <Route path="/Claim/:claimID" component={Claim} />
       <Route path="/SearchClaim" component={SearchClaim} />
     </Router>
   );

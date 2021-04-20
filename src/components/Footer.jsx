0b18a5e6113c184a696a36987c66663ad6ae244f
sujith <sujith0709@gmail.com>
2021-04-19 21:11:20 -0400

    added margin tofooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default function Navbar() {
   return (
-    <footer className="footer footer-thin">
+    <footer className="footer footer-thin" style={{ marginTop: "5rem" }}>
       <div className="container">
         <div className="row content-row">
           <div className="col-md-8 col-md-offset-2">
